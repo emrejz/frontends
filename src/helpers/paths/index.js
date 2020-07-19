50a0d615b4c27a402ad8249af86f966f9fbc8abe
emre kayacan <emre.kayacan@msn.com>
2020-07-20 00:42:37 +0300

    paths for convert css to scss

---
+++ src/helpers/paths/index.js
@@ -0,0 +1,8 @@
+module.exports = [
+  "src/index.js",
+  "src/components/header.js",
+  "src/pages/app/index.js",
+  "src/pages/content/index.js",
+  "src/pages/home/index.js",
+  ".storybook/config.js",
+];
