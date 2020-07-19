784ff7e65ad6fe231735db8a3a268798e63b3623
emre kayacan <emre.kayacan@msn.com>
2020-07-20 00:47:36 +0300

    main-bin index

---
+++ src/index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+const inquirer = require("inquirer");
+const fs = require("fs-extra");
+const questions = require("./helpers/questions");
+const createProject = require("./helpers/createProject");
+
+const buildConfig = async () => {
+  const answers = await inquirer.prompt(questions);
+  createProject(answers);
+};
+if (fs.readdirSync(__dirname).length !== 0) {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "overwrite",
+        message: "Directory is not empty! Would you like to overwrite it?",
+        default: false,
+      },
+    ])
+    .then((answers) => {
+      if (answers.overwrite) {
+        buildConfig();
+      } else {
+        console.log("Goodbye.");
+        process.exit(0);
+      }
+    });
+} else {
+  buildConfig();
+}
