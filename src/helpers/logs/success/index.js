8b2a10f72af1a3c551cea1fd3826cfde67f7233e
emre kayacan <emre.kayacan@msn.com>
2020-07-20 00:46:25 +0300

    success logs

---
+++ src/helpers/logs/success/index.js
@@ -0,0 +1,10 @@
+module.exports = () => {
+  console.log("\x1b[46m", "SUCCESS", "\x1b[0m");
+  console.log("\x1b[36m%s\x1b[0m", "\n       npm install");
+  console.log("\x1b[36m%s\x1b[0m", "\n       npm start");
+  console.log(
+    "\033[33m",
+    "\nContact me with this email when you find a problem or something else => emre.kayacan@msn.com"
+  );
+  console.log("\x1b[0m", "\nHappy coding..");
+};
