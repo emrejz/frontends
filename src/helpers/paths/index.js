2e569e971cb926f9f55375704ba40ebb9d71ad68
emre kayacan <emre.kayacan@msn.com>
2020-07-20 13:04:36 +0300

    new path added

--- src/helpers/paths/index.js
+++ src/helpers/paths/index.js
@@ -1,6 +1,6 @@
 module.exports = [
   "src/index.js",
-  "src/components/header.js",
+  "src/components/header/index.js",
   "src/pages/app/index.js",
   "src/pages/content/index.js",
   "src/pages/home/index.js",
