b3a0adc0f58ee866d8a5502c9b2442f137dd2b47
emre kayacan <emre.kayacan@msn.com>
2020-07-20 13:04:14 +0300

    return promise css to scss func

--- src/helpers/cssToScss/index.js
+++ src/helpers/cssToScss/index.js
@@ -1,10 +1,17 @@
 const fs = require("fs-extra");
 
 module.exports = (paths) =>
-  paths.forEach(async (path) => {
-    if (fs.existsSync(path)) {
-      const data = await fs.readFile(path, "utf8");
-      var result = data.replace(/.css/g, ".scss");
-      await fs.writeFile(path, result, "utf8");
-    }
+  new Promise((resolve, reject) => {
+    paths.forEach(async (path) => {
+      try {
+        if (fs.existsSync(path)) {
+          const data = await fs.readFile(path, "utf8");
+          var result = data.replace(/.css/g, ".scss");
+          await fs.writeFile(path, result, "utf8");
+          resolve("ok");
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
   });
